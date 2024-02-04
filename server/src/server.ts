92b3bc90d1945401d6866e779d30f5574fa0dee3
skifox52 <fet.a@hotmail.fr>
2024-02-04 21:39:04 +0100

    NVMRC

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,6 @@
-import fastify from "fastify"
+import Fastify from "fastify"
 import "dotenv/config"
 
-const server = fastify({ logger: true })
+const fastify = Fastify({ logger: true })
 
-server.listen({ port: +process.env.PORT! }, () =>
-  console.log(`App running on ${process.env.PORT}`)
-)
+fastify.listen({ port: +process.env.PORT!, host: "localhost" })
